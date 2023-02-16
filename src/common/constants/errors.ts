e9bb0c8996b05184eb73e74eb2a2a5d5d121aca7
TsimafeiBabrou <bobrovufmp@gmail.com>
2023-02-16 23:10:08 +0200

    first commit

---
+++ src/common/constants/errors.ts
@@ -0,0 +1,6 @@
+export const AppError = {
+  EMAIL_EXIST: "User with this email already exist",
+  USERNAME_EXIST: "User with this username already exist",
+  USER_NOT_EXIST: "User with this email not exist",
+  WRONG_DATA: "Wrong data"
+};
